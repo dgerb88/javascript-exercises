26ec3db946f8723208f6675aeb56656df9223f7b
dgerb88 <dax.gerber@gmail.com>
2025-07-22 08:00:29 -0600

    Finished up to 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats < 0) return "ERROR";
+    let returnString = ""
+    for (let i = 0; i < repeats; i++) {
+        returnString += string
+    }
+    return returnString
 };
 
 // Do not edit below this line
