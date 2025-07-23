2fae0c1a1e39976f24cd9edf3d41d77e8940eef4
dgerb88 <dax.gerber@gmail.com>
2025-07-23 15:57:51 -0600

    Finished 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let separated = stringToReverse.split('')
+    let reversed = separated.reverse()
+    return reversed.join('')
 };
 
 // Do not edit below this line
