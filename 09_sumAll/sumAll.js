9d05e7c791d0036facb088a5a74b2c118d215bff
dgerb88 <dax.gerber@gmail.com>
2025-07-30 08:08:54 -0600

    Finished 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+    if (!Number.isInteger(one) || !Number.isInteger(two)) { return "ERROR" }
+    if (one < 0 || two < 0) { return "ERROR" }
+    const step = one <= two ? 1 : -1
+    const length = Math.abs(two - one) + 1
+    const all = Array.from({ length }, (_, i) => one + i * step);
+    return all.reduce((sum, num) => sum + num, 0)
 };
 
 // Do not edit below this line
